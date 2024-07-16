f9a9b75b766ca2b255dcb851e2fd3b7d10f8bdbe
MCCLAB1200WL783 <MCCLAB1200WL783@usa.miracosta.cc.ca.us>
2024-07-16 14:45:48 -0700

    Includes the .gitignore file to ignore *.tmp files

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private Main() {
+    }
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how Android Studio suggests fixing it.
@@ -11,5 +14,11 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+    }
+
+    @androidx.annotation.NonNull
+    @org.jetbrains.annotations.Contract(value = " -> new", pure = true)
+    public static Main createMain() {
+        return new Main();
     }
 }
